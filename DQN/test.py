26a62fde6602c0ec7fadce10b7c88bd5983d7fa1
Vishnuteja-Surla <cs21b2037@iiitdm.ac.in>
2024-04-10 21:52:29 +0530

    Complete Code for DQN

---
+++ DQN/test.py
@@ -0,0 +1,23 @@
+import os
+import gym
+import numpy as np
+from PIL import Image
+import torch
+from breakout import *
+from model import AtariNet
+from agent import Agent
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+env = DQNBreakout(device=device, render_mode='human')
+
+model = AtariNet(nb_actions=4)
+model.to(device)
+model.load_model()
+
+agent = Agent(model=model, device=device, epsilon=0.05, nb_warmup=5000, nb_actions=4, learning_rate=0.00001,
+              memory_capacity=1000000, batch_size=64)
+
+agent.test(env=env)
